e2fe8e542a0eb43373421e562511292e44925311
Kaden Greenlief <kgreenl3@asu.edu>
2024-02-13 02:02:30 -0700

    😂😂😂Some more goofs and gafs😂😂😂

--- live-datadash/src-tauri/src/main.rs
+++ live-datadash/src-tauri/src/main.rs
@@ -1,6 +1,5 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
-
 //use core::num;
 use std::time::Duration;
 use serialport::SerialPort;
@@ -35,15 +34,19 @@ fn number_test(number: &str) -> String{
 fn serial_test(){
   let ports = serialport::available_ports().expect("No ports found!");
   let mut port_vec = vec![];
-  
+
   //Itterating though the available ports, printing then pushing them to port vector
   //  TODO: Could be possible to skip step and set port_vec equal to new serialport function
   for p in ports {
-      println!("{}", p.port_name);
+      //not implemented, looks to determine how the port is connected and give user more info on which one to choose
+      //Need a physical board to test
+      if(p.port_type == USBPort){
+        continue;
+      }
+      println!("[{}, {:?}]", p.port_name, p.port_type);
       port_vec.push(p.port_name);
   }
 
-  //TODO: have temp_port be a string that isn't pre-defined
   let mut temp_port = String::new();
   
   if port_vec.len() == 1 {
@@ -53,52 +56,54 @@ fn serial_test(){
   } else {
     //TODO: Prompt the user for what port to use 
     //implement serialport::SerialPortInfo.port_type or serialport::USBPortInfo (finding different info about ports to make it user friendly)
-    
-    println!("More than 1 port is being occupied, choose correct port");
-    print!("{:?}", port_vec);
+    println!("{:?}", port_vec);
+    println!("{}", port_vec[0]);
+    println!("More than 1 port is populated, choose correct port");
     let mut input = String::new();
-    io::stdin().read_line(&mut input);
+    let _ = io::stdin().read_line(&mut input);
     println!("");
     let chosen: usize = input.trim().parse().expect("Input not an integer");
-    println!("Testing {}", chosen);
     temp_port = port_vec[chosen].to_string();
-    //input.as_bytes().to_vec()[0]
     println!("Using port {}", temp_port);
   }
   
-  //opens serialport, needs to be tested on mac to see if open_native works
-  //println!("Using port {}", temp_port);
+  //opens serialport
+  //TODO: needs to be tested on mac to see if open_native works
   let mut port = serialport::new(temp_port, 115200)
-    .timeout(Duration::from_secs(1))
+    .timeout(Duration::from_secs(100))
     .open_native()
     .expect("Failed to open port");
 
-  //defining booleans outside thread to save memmory 
-  //possible it could not matter since it's being moved inside the thread
-
   let mut ready = port.write_data_terminal_ready(true);
-  let mut serial_buf: Vec<u8> = vec![0; 50];
+
+  //haven't calculated max length but in testing 50 chars was reached fairly quickly
+  let mut serial_buf: Vec<u8> = vec![0; 100];
+
   //Unsure if needed, implemented for debugging
-  //let mut cleared = port.clear(serialport::ClearBuffer::Input);
+  //let mut cleared = port.clear(serialport::ClearBuffer::All);
+
   let mut usable_data : Vec<Vec<i32>> = vec![];
-  //Runs as long as it can read data and another part of the program is running
+
+  
+  //Threads runs as long as it can read data and another part of the program is running
   thread::spawn(move || {
     while ready.is_ok() {
+
+      //Uses ASCII Chars, 48-57 = 0-9, 44 = ',', EOL = 13 = carriage return (\n)
+      //convert to char with "_ as char"
+
+      //Reads until buffer is full, if simulating padd at end of string after the \n
       port.read(serial_buf.as_mut_slice()).expect("Found no data!");
-      
-      //IMPORTANT: USES ASCII CHARS, 48-57 = 0-9, 44 = ',', EOL = 13 = carriage return (\n)
-      //convert to char with _ as char
-      //print!("index: {},", serial_buf[0]-48);
+
       usable_data.push(decode(serial_buf.to_vec()));
-      
-      //cleared = port.clear(serialport::ClearBuffer::Input);
       ready = port.write_data_terminal_ready(true);
+      
+      //cleared = port.clear(serialport::ClearBuffer::All);
     }
   });
 }
 
 fn decode(buf : Vec<u8>) -> Vec<i32>{
-  //returning vector
   let mut data: Vec<i32> = vec![];
 
   //tells program to increment index if i is a comma
@@ -107,7 +112,8 @@ fn decode(buf : Vec<u8>) -> Vec<i32>{
   let mut index = 0;
 
   for i in buf{
-    if i as char == '\n' {
+    //extra or is for testing without arduino, remove in final
+    if i as char == '\n' || i as char == '\\'{
       println!("final vec {:?}", data); 
       return data;
     } else if i as char == ' '{
@@ -117,14 +123,15 @@ fn decode(buf : Vec<u8>) -> Vec<i32>{
       index += 1;
     }else if i.is_ascii_digit() {
       if new_index{
+        //If previous character was a comma, this is a new number and needs to be pushed
         data.push((i - 48).into());
         new_index = false;
       } else {
+        //Increases previous number by a factor of ten and adds current value to it
         data[index] = data[index] * 10 + <u8 as Into<i32>>::into(i - 48);
       }
 
     }
-    //print!("{}", i as char);
   }
   return data;
 }
